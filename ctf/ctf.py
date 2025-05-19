0fb8a9ef3bb28611c1c7edde974ad77c36b347dd
FieryRMS <raiyanmohdshahrms@gmail.com>
2025-05-20 00:19:20 +0800

    update CTF discription

--- ctf/ctf.py
+++ ctf/ctf.py
@@ -3,7 +3,7 @@ from redbot.core import commands
 
 class CTF(commands.Cog, name="ctfcogs.CTF"):
     """
-    Commands that combine multiple cogs features together
+    Commands that streamline common flows by combining multiple cogs together
     """
 
     def __init__(self, bot: commands.Bot):
